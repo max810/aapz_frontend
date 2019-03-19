c53aace239cf84a200080a284e930c6b2116f34d
Maksym Bekuzarov <maksym.bekuzarov@nure.ua>
2019-03-19 02:06:15 +0200

    Final working prototype (videostream + inference)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,22 @@ import { Message } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
 
-  private _hubConnection: HubConnection;
   msgs: string[] = ["sads"];
   imgsrc = "";
+  clslabel = "None";
+  clr = "green";
+  class_names = [
+    '0_normal_driving',
+    '1_mobile_right',
+    '2_mobile_right_head',
+    '3_mobile_left',
+    '4_mobile_left_head',
+    '5_radio',
+    '6_drink',
+    '7_search',
+    '8_make_up',
+    '9_talking'
+  ]
   connection: HubConnection = new HubConnectionBuilder()
     .withUrl("http://localhost:5000/streamHub")
     .build();
@@ -24,26 +37,52 @@ export class AppComponent implements OnInit {
     const connect = this.connection;
     const parent = this;
 
+
     connect.start().then(function () {
+      // connect.stream("InferenceStream").subscribe({
+      //   next: (item) => {
+      //     console.log(item);
+      //     this.clslabel = item.toString();
+      //     this.clr = this.clslabel == "0" ? "green" : "red";
+      //   },
+      //   complete: () => {
+      //     console.log("finished classes.");
+      //   },
+      //   error: (err) => {
+      //     console.log(err);
+      //   }
+      // });
+
       connect.stream('VideoStream').subscribe({
         next: (item) => {
           // var image = new Image();
           parent.imgsrc = 'data:image/png;base64,' + item;
-          
+
           // console.log(item);
           // parent.msgs.push(item.toString());
         },
         complete: () => {
-          console.log("finished.");
+          console.log("finished video.");
         },
         error: (err) => {
           console.log(err);
         }
       });
-    })
+
+      console.log("video stream started");
+
+      connect.on("InferenceMessage", x => {
+        // console.log(x);
+        parent.clslabel = parent.class_names[x];
+        parent.clr = x == 0 ? "green" : "red";
+      });
+
+    });
+
+
   }
 
   ngOnInit(): void {
-  
+
   }
 }
